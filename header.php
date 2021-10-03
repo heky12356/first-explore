af87b16105d7f5f442b21cd896f2a69ec5f67d76
heky12356 <heji12356@gmail.com>
2021-10-03 10:44:18 +0800

    1

--- header.php
+++
@@ -1,6 +0,0 @@
-<li><a href="<? echo $siteurl; ?>">首页</a></li>
-<li><a href="<? echo $siteurl; ?>vip.php">申请收录</a></li>
-<li><a href="http://www.0558.la/download/" target="_blank"><font color="#FF0000">获取本站源码</font></a></li>
-<li><a href="<? echo $siteurl.$search; ?>/">查询是否收录</a></li>
-<li><a href="<? echo $siteurl; ?>news-1.html">快审服务</a></li>
-<li><a href="http://wpa.qq.com/msgrd?v=3&uin=344642362&Site=&Menu=yes" target="blank" rel="nofollow"><img border="0" src="<? echo $siteurl; ?>templets/default/css/qq.gif" ></a></li>
